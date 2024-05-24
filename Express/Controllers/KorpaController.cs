9626c6ee6c30d9ebf108e1696b05cb07385ad776
Your Name <you@example.com>
2024-05-24 21:37:54 +0200

    nez vise sta dodajem

---
+++ Express/Controllers/KorpaController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Express.Data;
+using Express.Models;
+
+namespace Express.Controllers
+{
+    public class KorpaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KorpaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Korpa
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Korpa.ToListAsync());
+        }
+
+        // GET: Korpa/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var korpa = await _context.Korpa
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (korpa == null)
+            {
+                return NotFound();
+            }
+
+            return View(korpa);
+        }
+
+        // GET: Korpa/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Korpa/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,idKorisnika,brojKartice")] Korpa korpa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(korpa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(korpa);
+        }
+
+        // GET: Korpa/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var korpa = await _context.Korpa.FindAsync(id);
+            if (korpa == null)
+            {
+                return NotFound();
+            }
+            return View(korpa);
+        }
+
+        // POST: Korpa/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,idKorisnika,brojKartice")] Korpa korpa)
+        {
+            if (id != korpa.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(korpa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KorpaExists(korpa.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(korpa);
+        }
+
+        // GET: Korpa/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var korpa = await _context.Korpa
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (korpa == null)
+            {
+                return NotFound();
+            }
+
+            return View(korpa);
+        }
+
+        // POST: Korpa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var korpa = await _context.Korpa.FindAsync(id);
+            if (korpa != null)
+            {
+                _context.Korpa.Remove(korpa);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KorpaExists(int id)
+        {
+            return _context.Korpa.Any(e => e.id == id);
+        }
+    }
+}
---
+++ Express/Models/Zaposlenik.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Express.Models
+{
+    public class Zaposlenik : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string ProfilePicture { get; set; }
+
+        // Additional properties
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Korpa Korpa { get; set; }
+
+        [ForeignKey("Proizvod")]
+        public int IdProizvoda { get; set; }
+
+        public Zaposlenik() { }
+
+        public Zaposlenik(string username, string email, string password, string firstName, string lastName, Korpa korpa, int idProizvoda)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Korpa = korpa;
+            IdProizvoda = idProizvoda;
+        }
+    }
+}
